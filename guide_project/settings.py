1973ead432d9c9de25a9d7579e1a76d9b8b71f64
HARIHARAN B P <bphariharan1301@gmail.com>
2023-03-02 12:28:21 +0530

    Deploy config

--- guide_project/settings.py
+++ guide_project/settings.py
@@ -206,5 +206,6 @@ EMAIL_USE_TLS = True
 # AWS S3 configs
 
 FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+# AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_STORAGE_BUCKET_NAME = 'django-guide-project'
 AWS_S3_REGION_NAME = 'ap-south-1'

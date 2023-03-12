69662acff90287f46b66383c2763c7b8cdc60f63
HARIHARAN B P <bphariharan1301@gmail.com>
2023-03-12 19:28:39 +0530

    Updates

--- guide_project/settings.py
+++ guide_project/settings.py
@@ -200,8 +200,8 @@ IMAGE_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Email Configuration
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'meantechofficial2906@gmail.com'
-EMAIL_HOST_PASSWORD = 'lrsadwnssgkhxcmi'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 
 LOGIN_URL = 'login'

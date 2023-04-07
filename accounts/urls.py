63583390b55ac78121200ca0cfa1cf4d7d80f8a5
HARIHARAN B P <bphariharan1301@gmail.com>
2023-04-07 22:34:16 +0530

    Feat: Password Reset and base.html added and extended

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,10 @@ urlpatterns = [
     path('guides-register', views.guides_register, name='guides-register'),
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
+
+    path("password-reset/", views.password_reset, name="password-reset"),
+    path("password-reset/done/", views.password_reset_done,
+         name="password-reset-done"),
+    path("reset/<uidb64>/<token>/", views.password_reset_confirm,
+         name="password-reset-confirm"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -5,11 +5,17 @@ from django.contrib.auth.models import User, auth
 from django.contrib.auth import get_user_model
 from django.contrib import messages
 from pages.models import Guide, Team, Otp_Two, Temp_Team
-from django.contrib.auth.password_validation import MinimumLengthValidator, NumericPasswordValidator, validate_password
-from django.core.exceptions import ValidationError
+from django.template import loader
+from django.contrib.sites.shortcuts import get_current_site
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.core.mail import EmailMultiAlternatives
 
 from verify_email.email_handler import send_verification_email
 from .forms import GuideSignUpForm, StudentSignUpForm
+from guide_project.settings import EMAIL_HOST_USER
+
 
 UserModel = get_user_model()
 
@@ -225,3 +231,119 @@ def logout(request):
     auth.logout(request)
     messages.success(request, 'You are successfully logged Out and can login!')
     return redirect('login')
+
+# For password reset
+
+
+def password_reset(request):
+    email_template_name = 'registration/password_reset_email.html'
+    use_https = False
+    if request.method == 'POST':
+        email = request.POST['email']
+        teamID = request.POST['teamID']
+        if User.objects.filter(username=teamID).exists():
+            user = User.objects.filter(username=teamID).get()
+            user.email = email
+            subject = "PASSWORD REST FOR PROJECT REGISTRATION"
+            current_site = get_current_site(request)
+            domain = current_site.domain
+            site_name = current_site.name
+            token_generator = default_token_generator
+            if request.is_secure():
+                use_https = True
+            context = {
+                "email": email,
+                "domain": domain,
+                "site_name": site_name,
+                "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                "user": user,
+                "token": token_generator.make_token(user),
+                "protocol": "https" if use_https else "http",
+            }
+            body = loader.render_to_string(email_template_name, context)
+            email_message = EmailMultiAlternatives(
+                subject, body, EMAIL_HOST_USER, [email]
+            )
+            email_message.send()
+            return redirect('password-reset-done')
+
+        else:
+            messages.error(
+                request, "User does not exist with that teamID/email!")
+            return redirect('password_reset')
+    else:
+        return render(request, 'registration/password_reset_form.html')
+
+
+def password_reset_done(request):
+    return render(request, 'registration/password_reset_done.html')
+
+
+def password_reset_confirm(request, uidb64, token):
+    validlink = False
+    token_generator = default_token_generator
+    uid = urlsafe_base64_decode(uidb64).decode()
+    user = UserModel._default_manager.get(pk=uid)
+    if token_generator.check_token(user, token):
+        validlink = True
+    if request.method == 'POST':
+        new_password1 = request.POST['new_password1']
+        new_password2 = request.POST['new_password2']
+        if new_password1 == new_password2:
+            special_characters = "[~\!@#\$%\^&\*\(\)_\+{}\":;'\[\]]"
+            if len(new_password1) < 8:
+                messages.error(
+                    request, 'Password length must be atleast 8 character.')
+                return redirect('password-reset-confirm', uidb64, token)
+
+            # Check for dig
+            if not any(char.isdigit() for char in new_password1):
+                messages.error(
+                    request, 'Password must contain at least 1 digit.')
+                return redirect('password-reset-confirm', uidb64, token)
+
+            # Check for alpha
+            if not any(char.isalpha() for char in new_password1):
+                messages.error(
+                    request, 'Password must contain at least 1 letter and must be alpha-numeric.')
+                return redirect('password-reset-confirm', uidb64, token)
+
+            # Check spl char
+            if not any(char in special_characters for char in new_password1):
+                messages.error(
+                    request, 'Password must contain at least 1 special character')
+                return redirect('password-reset-confirm', uidb64, token)
+
+            # if Guide.objects.filter(emp_id=teamID).exists():
+            #     if User.objects.filter(username=email).exists():
+            #         pass
+            # else:
+            #     return redirect('login')
+
+            # Check for user existence
+            # temp = int(teamID)
+            print('id is: ', id)
+            if Guide.objects.filter(email=user.email).exists():
+                if User.objects.filter(username=user.email).exists():
+                    user = User.objects.filter(email=user.email).get()
+                    user.set_password(new_password1)
+                    user.save()
+                    messages.success(request, 'Password Changed successfully!')
+                    return redirect('login')
+            elif User.objects.filter(email=user.email).exists():
+                user = User.objects.filter(email=user.email).get()
+                user.set_password(new_password1)
+                user.save()
+                messages.success(request, 'Password Changed successfully!')
+                return redirect('login')
+        else:
+            messages.error(request, 'Password not matching!')
+            return redirect('password-reset-confirm', uidb64, token)
+    else:
+        context = {
+            'user': user,
+            'uidb64': uidb64,
+            'token': token,
+            'validlink': validlink
+        }
+        return render(request, 'registration/password_reset_confirm.html', context)

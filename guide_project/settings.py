c347e72d6dda7238fe7394cad32b34cda2ab0621
HARIHARAN B P <bphariharan1301@gmail.com>
2023-04-22 12:37:07 +0530

    Update: Settings change

--- guide_project/settings.py
+++ guide_project/settings.py
@@ -30,8 +30,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-bn!gb+_&96p0e7j+0@in%!o5xxl23m$907%3sgyqlekw&85sgv'
 SECRET_KEY = os.environ.get('SECRET_KEY')
-# DEBUG = os.environ.get('DEBUG')
-DEBUG = False
+DEBUG = os.environ.get('DEBUG')
+# DEBUG = False
 
 # DATABASE_URL = "postgresql://postgres:4M84u0U9dcm9LmdaH2Ar@containers-us-west-105.railway.app:5862/railway"
 
@@ -150,8 +150,13 @@ WSGI_APPLICATION = 'guide_project.wsgi.application'
 
 # Production DB to be used for railway personal
 
-DATABASES = {
+DATABASE_URL = "postgresql://postgres:M5IdDWcXIAhyt06Sh0w0@containers-us-west-132.railway.app:6989/railway"
+
+'''DATABASES = {
     "default": dj_database_url.config(default=os.environ.get('DATABASE_URL'), conn_max_age=None),
+}'''
+DATABASES = {
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=None),
 }
 
 # Password validation

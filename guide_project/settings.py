92a18acd2b937fbe70772d9aab731ca50cda7759
HARIHARAN B P <bphariharan1301@gmail.com>
2023-04-25 10:27:21 +0530

    Update: Demo Video enabled

--- guide_project/settings.py
+++ guide_project/settings.py
@@ -261,5 +261,5 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # AWS_STORAGE_BUCKET_NAME = 's3-django-test-bucket'
 # Logout On Closing
-# SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
-# SESSION_EXPIRE_AT_BROWSER_CLOSE = False
+SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+SESSION_EXPIRE_AT_BROWSER_CLOSE = False
